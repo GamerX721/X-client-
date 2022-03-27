e8d3615d75d002de23bee91cbf461b143e1d855c
GamerX721 <101965681+GamerX721@users.noreply.github.com>
2022-03-27 09:04:01 +0800

    Add files via upload

---
+++ app/features/features.js
@@ -0,0 +1,8 @@
+module.exports = {
+    keystrokes: require('./keystrokes.js'),
+    //skyColor: require('./skyColor.js')
+    menuTimer: require('./menuTimer.js'),
+    runRpc: require('./discord-rpc.js')
+    //quickJoin: require('./quickJoin.js'),
+    //badges: require('./badges.js'),
+}
