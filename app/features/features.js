dc74cabbe257b48223d8841ccc992df3a9ed8dba
GamerX721 <101965681+GamerX721@users.noreply.github.com>
2022-03-27 09:24:55 +0800

    Delete app directory

--- app/features/features.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    keystrokes: require('./keystrokes.js'),
-    //skyColor: require('./skyColor.js')
-    menuTimer: require('./menuTimer.js'),
-    runRpc: require('./discord-rpc.js')
-    //quickJoin: require('./quickJoin.js'),
-    //badges: require('./badges.js'),
-}
